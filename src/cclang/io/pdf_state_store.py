284373ad0b4135937dbc6b5fb4b23963963e1d39
Pavel Turaev <pnturaev@edu.hse.ru>
2025-11-28 12:06:45 +0300

    complete model storing pdf state

--- src/cclang/io/pdf_state_store.py
+++ src/cclang/io/pdf_state_store.py
@@ -1,7 +1,7 @@
 import threading
 from datetime import datetime
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Iterable
 import sqlite3
 
 from cclang.io.schemas import PdfState, ProcessingStatus
@@ -99,4 +99,67 @@ class PdfStateStore:
             )
             self._conn.commit()
 
-    # TODO
+    def has_text_sha(self, text_sha: str) -> bool:
+        with self._lock:
+            if self._conn is None:
+                raise RuntimeError('invalid acces to closed connection')
+            cur = self._conn.execute(
+                """
+                SELECT EXISTS(SELECT 1 FROM pdf_state WHERE text_sha = ?)
+                LIMIT 1
+                """, (text_sha,)
+            )
+            return cur.fetchone()[0]
+
+    def has_lemma_sha(self, lemma_sha: str) -> bool:
+        with self._lock:
+            if self._conn is None:
+                raise RuntimeError('invalid acces to closed connection')
+            cur = self._conn.execute(
+                """
+                SELECT EXISTS(SELECT 1 FROM pdf_state where lemma_sha = ?)
+                LIMIT 1
+                """, (lemma_sha,)
+            )
+            return cur.fetchone()[0]
+
+    def filter_by_text_status(self, text_status: ProcessingStatus | None) -> Iterable[PdfState]:
+        # !without streaming, because of lifetime of cursor
+        with self._lock:
+            if self._conn is None:
+                raise RuntimeError('invalid acces to closed connection')
+            cur = self._conn.execute(
+                """SELECT pdf_sha,
+                          pdf_path,
+                          text_sha,
+                          text_path,
+                          text_status,
+                          text_updated_at,
+                          lemma_sha,
+                          lemma_path,
+                          lemma_status,
+                          lemma_updated_at
+                   FROM pdf_state
+                   WHERE text_status = ?
+                """, (text_status,))
+            return map(_pdf_state_from_db_resp, cur.fetchall())
+
+    def filter_by_lemma_status(self, lemma_status: ProcessingStatus | None) -> Iterable[PdfState]:
+        with self._lock:
+            if self._conn is None:
+                raise RuntimeError('invalid acces to closed connection')
+            cur = self._conn.execute(
+                """SELECT pdf_sha,
+                          pdf_path,
+                          text_sha,
+                          text_path,
+                          text_status,
+                          text_updated_at,
+                          lemma_sha,
+                          lemma_path,
+                          lemma_status,
+                          lemma_updated_at
+                   FROM pdf_state
+                   WHERE lemma_status = ?
+                """, (lemma_status,))
+            return map(_pdf_state_from_db_resp, cur.fetchall())
